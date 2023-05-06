1a05ea8d8b4232b9d4aaee1793d66e20bb1fa228
Arad Afzali <arad.h.a.afzali@gmail.com>
2023-05-06 23:24:22 +0330

    Added dashboard

---
+++ Nura Bank/js/dashboard-js.js
@@ -0,0 +1,14 @@
+let btn = document.querySelector("#btn");
+let colleft = document.querySelector(".column-left");
+let colmid = document.querySelector(".column-middle");
+let colright = document.querySelector(".column-right");
+
+function myFunction () {
+  colleft.classList.toggle("active");
+  colmid.classList.toggle("active");
+  colright.classList.toggle("active");
+};
+
+
+
+
